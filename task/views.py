f897f7f7b85783b5998f494b166646d24b1d17cf
Ewerton <ewertonferreirasantiago@gmail.com>
2017-02-20 21:15:33 -0300

    projeto

---
+++ task/views.py
@@ -0,0 +1,21 @@
+__autor__ = 'ESANTIAGO'
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+class Tarefa(object):
+    def __init__(self, titulo, data):
+        self.titulo = titulo
+        self.data = data
+
+    def __str__(self):
+        return self.titulo
+
+def home(request):
+    return HttpResponse("Olá")
+
+def sobre(request):
+    return HttpResponse("Ewerton Santiago")
+
+def tarefa(request, ano, mes):
+    return HttpResponse("tarefa:" + str(ano) + "/" + str(mes))
